fe3b17bbe7215f90fc1ee0201b503919912bec0f
rdgrb <rdo.rodrigo2@gmail.com>
2020-10-10 20:31:36 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+# SQL - AC02 
+## Enunciado 
+Utilizando qualquer linguagem de programação escolhida, realize a seguinte atividade:
+- Criar um programa que conecte ao Banco de Dados (SQL) com a linguagem; 
+- O programa deverá ler um arquivo de entrada e inserir os registros em uma tabela SQL; 
+- Dados do arquivo de entrada: CPF ou CNPJ, NOME, PONTUAÇÃO, TELEFONE, COD_PROBLEMA e OBSERVAÇÕES 
+- Dados do Database (duas tabelas): 
+-- CLIENTES: Com os dados cadastrais citados acima 
+-- PROBLEMAS: Apenas COD_PROBLEMA e DESCRIÇÃO
---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+    frame: false,
+  })
+
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
